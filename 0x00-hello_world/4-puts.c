54d8e0f498453a294b64564fe73fe3d125d90100
Jeyrio <j.atueyi@yahoo.com>
2023-09-08 12:44:35 +0000

    done with 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char MyMessage[] = "Programming is like building a multilingual puzzle";
+
+	puts(MyMessage);
+
+	return (0);
+}
