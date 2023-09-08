e4707ebebbeb1460e85fccb5f959705528d93850
Jeyrio <j.atueyi@yahoo.com>
2023-09-08 13:52:02 +0000

    done with 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char MyMessage[] = "Programming is like building a multilingual puzzle";
+	const char *MyMessage = "Programming is like building a multilingual puzzle";
 
 	puts(MyMessage);
 
