2ba60ddf5b7eda6e9b754078a2ef455aafcdd0ef
Jeyrio <j.atueyi@yahoo.com>
2023-10-10 16:52:56 +0000

    done with 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j <= 90)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
