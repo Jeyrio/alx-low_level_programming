84d0fd73df712ea85e92948fec0d9a359df6c6b3
Jeyrio <j.atueyi@yahoo.com>
2023-10-23 18:16:46 +0000

    done with 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Function that adds two integers
+ * @n: The character to be checked
+ * @m: The character to be checked
+ * Return: The result
+ *
+ */
+int add(int n, int m)
+{
+	int add;
+
+	add = n + m;
+
+	return (add);
+}
