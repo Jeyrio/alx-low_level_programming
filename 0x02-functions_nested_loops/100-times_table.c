9135a704b13f562be525674922250225e8fa495d
Jeyrio <j.atueyi@yahoo.com>
2023-10-27 19:28:11 +0000

    done with 100-time_table.c file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * print_times_table - Function that prints the n times table, starting with 0
+ * @n: The character to be checked
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_times_table(int n)
+{
+	int row, column, result;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+
+	for (row = 0; row <= n; row++)
+	{
+		_putchar('0');
+		for (column = 0; column <= n; column++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = row * column;
+
+			if (result <= 9)
+			{
+				_putchar(' ');
+			}
+
+			if (result <= 99)
+			{
+				_putchar(' ');
+			}
+
+			if (result >= 100)
+			{
+				_putchar((result / 100) + '0');
+				_putchar((result / 10) % 10 + '0');
+			}
+			else if (result <= 99 && result >= 10)
+			{
+				_putchar((result / 10) + '0');
+			}
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
