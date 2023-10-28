867f803c2b0f3972f23550bdd176007f89445f02
Jeyrio <j.atueyi@yahoo.com>
2023-10-28 10:55:58 +0000

    done with 101-natural.c file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	int sum = 0;
 	int limit = 1024;
+	int i;
 
-	for (int i = 1; i < limit; i++)
+	for (i = 1; i < limit; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
@@ -19,7 +20,7 @@ int main(void)
 		}
 	}
 
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
