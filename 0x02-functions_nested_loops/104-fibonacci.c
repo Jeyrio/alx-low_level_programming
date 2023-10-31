c13b06ca266d7ad84a52e43d6d45c3db3a320bdb
Jeyrio <j.atueyi@yahoo.com>
2023-10-31 02:05:34 +0000

    done with 104-fibonacci .c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	long int fib1 = 1;
-	long int fib2 = 2;
-	long int next;
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+	unsigned long int next;
 	int n;
 
-	printf("%ld, %ld, ", fib1, fib2);
+	printf("%lu, %lu, ", fib1, fib2);
 
 	for (n = 3; n <= 98; n++)
 	{
 		next = fib1 + fib2;
-		printf("%ld", next);
+		printf("%lu", next);
 
 		if (n < 98)
 		{
