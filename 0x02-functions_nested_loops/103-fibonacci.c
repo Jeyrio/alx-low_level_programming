66c375573a5891f846e80a7a89caf81a6f2265d6
Jeyrio <j.atueyi@yahoo.com>
2023-10-31 00:27:09 +0000

    done with 103-fibonacci .c file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	long int fib1 = 1;
+	long int fib2 = 2;
+	long int next;
+	int sum = 2;
+
+	while (1)
+	{
+		next = fib1 + fib2;
+
+		if (next > 4000000)
+		{
+			break;
+		}
+
+		if (next % 2 == 0)
+		{
+			sum = sum + next;
+		}
+
+		fib1 = fib2;
+		fib2 = next;
+	}
+	printf("Sum of even Fibonacci terms not exceeding 4,000,000 is: %ld\n", sum);
+
+	return (0);
+}
