f18de74d21676cb4d81e52fe71bbcd21d96cd3ab
Jeyrio <j.atueyi@yahoo.com>
2023-11-05 11:38:06 +0000

    done with task 0 of debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,10 +2,9 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* @n; The integer to be checked
+* @i: The integer to be checked
 * Return: 0
 */
-void positive_or_negative(int n);
 
 int main(void)
 {
