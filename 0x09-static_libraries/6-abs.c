9919c18b0dbc902309ea4df56e5a424af7296d2b
Jeyrio <j.atueyi@yahoo.com>
2023-11-21 04:14:27 +0000

    Done with creating libmy.a, main.h file

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Function that computes the absolute value of an integer
+ *
+ * @n: The number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
