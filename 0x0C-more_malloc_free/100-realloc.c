79b9f174050541728c2aa717ff0d2de0ef6323f2
Jeyrio <j.atueyi@yahoo.com>
2023-12-28 14:38:09 +0000

    done with 100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *source, *destination;
 	unsigned int i;
 
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 	if (new_size == 0)
 	{
 		free(ptr);
@@ -25,10 +29,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
-	}
-	if (new_size == old_size)
-	{
-		return (ptr);
 	}
 
 	ptr1 = malloc(new_size);
