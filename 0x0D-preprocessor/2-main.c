0a9e2aaede6ecdbb77fb6a83e1b0a91806bf92e4
Jeyrio <j.atueyi@yahoo.com>
2023-12-30 20:34:00 +0000

    done with 3-function_like_macro.h file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - program that prints the name of the file
- * it was compiled from, followed by a new line.
+ t am i* it was compiled from, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
