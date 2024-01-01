c91677cb40b54d8f694eeed36339049ca2030b93
Jeyrio <j.atueyi@yahoo.com>
2024-01-01 16:24:33 +0000

    done with dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Function that define the struct dog
+ * which include the member of name, age, owner
+ *
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
 struct dog
 {
 	char *name;
