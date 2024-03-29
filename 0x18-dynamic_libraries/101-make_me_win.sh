ffdb6f0b038663480ca3fffb9c49a9230ef7ad63
Jeyrio <j.atueyi@yahoo.com>
2024-03-29 18:07:45 +0000

    hack file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -P . https://github.com/Jeyrio/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+wget -P . https://github.com/Jeyrio/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
